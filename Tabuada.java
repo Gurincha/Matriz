8ae7e7c8f94b799965a43dcf98d85a49edc6b3f4
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-03-21 17:50:37 +0000

    att

--- Tabuada.java
+++ Tabuada.java
@@ -9,6 +9,7 @@ public class Tabuada {
             idade[cont] = cont*10;
         }
         //for (int = cont:idade) funciona igual o for ->
+        //System.out.println(cont);
         for (int cont = 0; cont < idade.length;cont ++){
 
             System.out.println(idade[cont]);

337b1a7c366b732f9f930a1c12215535ed42013e
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-03-21 17:43:37 +0000

    att

---
+++ Tabuada.java
@@ -0,0 +1,17 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        
+        int [] idade;
+        idade = new int[11];
+
+
+        for (int cont = 0; cont < idade.length;cont ++){
+            idade[cont] = cont*10;
+        }
+        for (int cont = 0; cont < idade.length;cont ++){
+
+            System.out.println(idade[cont]);
+        }
+    }
+}
+
